d67edf33e420276345394b5fb669c126a99454e0
DevilMex <luca.de.vito.ldv@gmail.com>
2020-09-23 15:46:57 +0200

    Miglioramento versione Alpha

--- app/src/main/java/com/example/onepiecefinder/MainActivity.java
+++ app/src/main/java/com/example/onepiecefinder/MainActivity.java
@@ -7,11 +7,16 @@ import android.app.Notification;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.onepiecefinder.classes.Episode;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,35 +25,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText episodeNumber = findViewById(R.id.episodeNumber);
+//        episodeNumber.set("");
 
         Button button = findViewById(R.id.mirror1button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText episodeView = findViewById(R.id.episodeNumber);
-                CharSequence episodeCharSeq = episodeView.getText();
+                TextView episodeFieldView = findViewById(R.id.episodeField);
+                EditText episodeNumberView = findViewById(R.id.episodeNumber);
+                CharSequence episodeCharSeq = episodeNumberView.getText();
                 if(!episodeCharSeq.toString().equals("")){
-                    int episode = Integer.parseInt(episodeCharSeq.toString());
-                    if(episode == 0) {
-                        sendNotification();
+                    Episode episode = new Episode(Integer.parseInt(episodeCharSeq.toString()));
+                    if(episode.getNumber() <= 0) {
+                        sendNotification("Valore non previsto", "L'episodio " + episode.getNumber() + " risulta inesistente!");
+                        episodeFieldView.setText("Valore non previsto! L'episodio " + episode.getNumber() + " risulta inesistente!");
                     } else {
-                        Log.i("Riconoscimento valore", "creazione della pagina");
-                        //Mettere webView in app
+                        Log.i("Valore accettato!", "Creazione dei links...");
+                        episodeFieldView.setMovementMethod(LinkMovementMethod.getInstance());
+                        episodeFieldView.setText(Html.fromHtml("" +
+                                "<h1><a href=\"" + episode.getFirstLink() + "\">Link 1</a></h1>" +
+                                "<h1><a href=\"" + episode.getSecondLink() + "\">Link 2</a></h1>"
+                        ));
                     }
                 } else {
-                    sendNotification();
+                    sendNotification("Contenuto nullo!", "Non c'è nulla");
+                    episodeFieldView.setText("Contenuto nullo! il campo è vuoto!");
                 }
             }
         });
     }
 
-    void sendNotification(){
-        Log.w("Input Error","Errore nell'input. Creazione notifica");
+    void sendNotification(String title, String msg){
+        Log.w("Input Error","Errore nell'input. Creazione notifica...");
         NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this)
                 .setSmallIcon(R.drawable.ic_alert)
-                .setContentTitle("Message")
-                .setContentText("Text text")
+                .setContentTitle(title)
+                .setContentText(msg)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
     }
 
---
+++ app/src/main/java/com/example/onepiecefinder/classes/Episode.java
@@ -0,0 +1,49 @@
+package com.example.onepiecefinder.classes;
+
+public class Episode {
+
+    protected int number;
+    protected String stringNumber;
+    protected String firstLink;
+    protected String secondLink;
+
+    public Episode(){}
+
+    public Episode(int number){
+        this.number = number;
+        stringNumber = getStringNumber(number);
+        firstLink = getFirstLink();
+        secondLink = getSecondLink();
+    }
+
+    public String getStringNumber() {
+        return stringNumber;
+    }
+
+    public String getFirstLink(){
+        return "https://www.animehdita.org/host/srv0.php?v=OnePieceITA/OnePiece_Ep_" + stringNumber + "_ITA";
+    }
+
+    public String getSecondLink(){
+        return "https://www.animehdita.org/host/srv0.php?v=OnePiece/OnePiece_Ep_" + stringNumber + "_ITA";
+    }
+
+    private String getStringNumber(int num){
+        if(num < 10){
+            return "00" + num;
+        } else if(num < 100){
+            return "0" + num;
+        } else {
+            return "" + num;
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+}
